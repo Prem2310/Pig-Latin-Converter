e1eef2d892911aec3130ae8a0ba45f349444186b
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2023-04-20 21:40:50 +0530

    Add files via upload
    new

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+  fetch("https://pig-latin.patelprem1.repl.co/api?sentence=" + input.value)
+    .then((res) => res.json())
+    .then((data) => {
+      output.innerHTML = data.output;
+    });
+});
